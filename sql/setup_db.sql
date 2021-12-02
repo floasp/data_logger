3e2d5a506e3fdc7db93c8fe54fb4a8d2eba7eb56
Florian Asperger <flo.asperger@gmail.com>
2021-12-02 18:06:43 +0100

    added SQL code.

---
+++ sql/setup_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `datatypes` (
+ `typeID` int(11) NOT NULL AUTO_INCREMENT,
+ `datatype` varchar(32) NOT NULL,
+ `name` varchar(32) NOT NULL,
+ `unit` varchar(16) NOT NULL,
+ PRIMARY KEY (`typeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4
+
+CREATE TABLE `ID_list` (
+ `ID` int(11) NOT NULL AUTO_INCREMENT,
+ `token` varchar(16) NOT NULL,
+ `name` varchar(64) NOT NULL,
+ `short_name` varchar(5) NOT NULL,
+ `datatypes` varchar(255) NOT NULL,
+ PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4
